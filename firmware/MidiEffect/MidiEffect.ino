4568eb08616dc925f87aecfe22dc9b47cf0a2713
Marius Kintel <marius@kintel.net>
2014-07-10 17:31:14 +0200

    Added igniter control and manual serial port control

--- firmware/MidiEffect/MidiEffect.ino
+++ firmware/MidiEffect/MidiEffect.ino
@@ -1,6 +1,9 @@
 #include <MIDI.h>
 
 int valvePins[4] = {11,12,13,14};
+int igniterPin = 10;
+long lastEffect = 0;
+long igniterSafetyTimeout = 10000; // 10 sec
 
 struct Valve {
   int pin;
@@ -16,6 +19,8 @@ struct Valve {
     for (int i=0;i<4;i++) {
       if (valves[i].timeout <= m) valves[i].off();
     }
+
+    if (m - lastEffect > igniterSafetyTimeout) digitalWrite(igniterPin, 0);
   }
 
   static Valve valves[4];
@@ -61,6 +66,8 @@ void handleNoteOn(byte channel, byte pitch, byte velocity) {
   if (effect >= 0) {
     Serial.print("Effect On: ");
     Serial.println(effect, DEC);
+    digitalWrite(igniterPin, 1);
+    lastEffect = millis();
 
     int duration = map(velocity, 0, 127, 0, 1000);
     Valve::valves[effect].timeout = millis() + duration;
@@ -82,11 +89,14 @@ void setup()
     Valve::valves[i].pin = valvePins[i];
     pinMode(Valve::valves[i].pin, OUTPUT);
   }
+  pinMode(igniterPin, OUTPUT);
+  digitalWrite(igniterPin, 0);
 
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
 
   MIDI.begin(MIDI_CHANNEL_OMNI);
+  pinMode(CORE_RXD1_PIN, INPUT_PULLUP);
 
   Serial.begin(9600);
   Serial.println("Hello MIDI effect");
@@ -96,4 +106,16 @@ void loop()
 {
   MIDI.read();
   Valve::checkTimeouts();
+
+  // Manual mode
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c >= '1' && c <= '4') {
+      int effect = c - '1';
+      Valve::valves[effect].timeout = millis() + 300;
+      Valve::valves[effect].on();
+      digitalWrite(igniterPin, 1);
+      lastEffect = millis();
+    }
+  }
 }
